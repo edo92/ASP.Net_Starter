95b2691e53c0420c40939707da86a1cd5d343304
edo92 <edoter92@gmail.com>
2024-02-10 07:29:38 -0800

    setup docker file

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c74c1128357c41ad3f91cef77ad6cad9c6ccbf7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a1d1898dc252539cdfe942af31c40309a6ce276")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
