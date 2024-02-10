d09f5900914878a827e5dc62bdc15139c17f73ff
edo92 <edoter92@gmail.com>
2024-02-10 04:46:33 -0800

    database context setup

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4025a6f1afbee7129dac74fcaf6fa85049aaea84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e48d6156b9e05affb7cce56ae0363d41c12fad7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
