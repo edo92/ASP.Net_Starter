08ef691d698908f73223a0aa47141aefb4532b96
edo92 <edoter92@gmail.com>
2024-02-11 05:04:01 -0800

    command service controller setup

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        public PlatformController()
+        {
+
+        }
+
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST http request");
+            return Ok("Inbound test of from platforms controller");
+        }
+    }
+}
